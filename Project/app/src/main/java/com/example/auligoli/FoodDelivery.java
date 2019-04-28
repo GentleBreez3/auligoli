7ca9b32d9a9030adcdd0d1cd66f5de1261c0a469
sCar3Cr0w25 <wretchedegg8@gmail.com>
2019-04-29 00:56:13 +0600

    fire service commit

---
+++ Project/app/src/main/java/com/example/auligoli/FoodDelivery.java
@@ -0,0 +1,13 @@
+package com.example.auligoli;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class FoodDelivery extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_food_delivery);
+    }
+}
