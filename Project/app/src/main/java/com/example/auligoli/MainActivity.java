f334861d306c0057c674028d2c16634b596bb0ee
sCar3Cr0w25 <wretchedegg8@gmail.com>
2019-05-06 17:23:48 +0600

    resolved issues

---
+++ Project/app/src/main/java/com/example/auligoli/MainActivity.java
@@ -0,0 +1,43 @@
+package com.example.auligoli;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+
+    public void hospitalService(View view) {
+        Button btnHospital=(Button) findViewById(R.id.hospital_service_button);
+        Intent intent=new Intent(MainActivity.this,HospitalService.class);
+        startActivity(intent);
+    }
+
+    public void ambulanceService(View view) {
+        Button btnHospital=(Button) findViewById(R.id.ambulance_service_button);
+        Intent intent=new Intent(MainActivity.this,AmbulanceService.class);
+        startActivity(intent);
+    }
+
+    public void fireService(View view) {
+        Button btnHospital=(Button) findViewById(R.id.fire_service_button);
+        Intent intent=new Intent(MainActivity.this,FireService.class);
+        startActivity(intent);
+    }
+
+
+    public void restaurantService(View view) {
+        Button btnHospital=(Button) findViewById(R.id.restaurant_service_button);
+        Intent intent=new Intent(MainActivity.this,RestaurantService.class);
+        startActivity(intent);
+    }
+
+}
